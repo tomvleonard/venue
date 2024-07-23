b54e0d3f39609303f2bba732cf5cfe150eca32d6
Tom Leonard <tvleonard@gmail.com>
2024-07-23 13:56:20 -0400

    Add db seeds for dev environment.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+if Rails.env.development? # Only run in development environment!
+  unless User.exists?(username: 'admin1')
+    admin = User.create(
+      username: 'admin1',
+      email: 'admin@venue.com',
+      password: '!s3crEt',
+      password_confirmation: '!s3crEt'
+    )
+  end
+
+  if Event.count.zero?
+    # creates past, present and future events
+    [ -60, -30, -20, -5, -1, 0, 1, 5, 20, 30, 60 ].each_with_index do |days, index|
+      date = Date.current + days
+      Event.create(
+        performer: "Performer #{index + 1}",
+        when: DateTime.new(date.year, date.month, date.day, 20, 00, 00),
+        content: "This is the event content for Performer #{index + 1}"
+      )
+    end
+  end
+end
