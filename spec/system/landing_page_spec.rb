4601c55a8fa7dc5fc24e4add75876e41efb8e8a7
Tom Leonard <tvleonard@gmail.com>
2024-07-24 11:10:14 -0400

    Add system spec test stub for landing page.

---
+++ spec/system/landing_page_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'Landing Page', :system do
+  describe 'when a user navigates to root' do
+    xit 'renders home#index' do
+      # we don't have a default root path yet
+    end
+  end
+end
